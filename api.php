6b977074e0c8d9aba82db1c4b514a9f6cb4da231
Sebastiano90 <sebastiannieblas23@gmail.com>
2026-09-23 07:07:00 -0700

    modifico errores de linea 12 codigo.js, se realizo nueva implementacion por lo que se cambio la URL de apps scripts en api.php

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 // api.php
 <?php
-const URL_APPSCRIPT = 'https://script.google.com/macros/s/AKfycbwTC8hYh1b0FV6ROiGgPgHh2j-qW4zV8USLrtcv6juHNYgcdCx4OqAorHHLPXr2M5w9/exec';
+const URL_APPSCRIPT = 'https://script.google.com/macros/s/AKfycbwjgEMDzNNZ8MxEfXkzLR5xCSFryOjZmTSluyy0O2S5rDblmy8fKsqLXsCszyxy523D/exec';
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
