41a95a99794abfcceab960e45df6db0e832252ca
Sebastiano90 <sebastiannieblas23@gmail.com>
2026-09-23 19:06:55 -0700

    EL PROGRAMA FUNCIONO, este cambio es cambio de la cuenta de sebastian a webmasterregistro (script google y sheets)

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 
 <?php
-const URL_APPSCRIPT = 'https://script.google.com/macros/s/AKfycbyw4MfcQQyaAHosIfQ9kLxyHi-XxF5Ial57MVv7jKlS9QZrt5SIC_HGXRWEAw0InLbv/exec';
+const URL_APPSCRIPT = 'https://script.google.com/macros/s/AKfycbzAvMkENlbnj5dKE7CEo23PNaQ5TLX29j629VvhdBZeJYru0xw9HMhe1WPGyOYIezS1/exec';
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
